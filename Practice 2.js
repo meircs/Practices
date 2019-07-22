3e398a7671cdd6dcaaf8d5a845cc88dcf4489c0f
Meiirkhan Zhalel <meiirkhan.zhalel@nu.edu.kz>
2019-07-22 23:09:23 +0600

    adding 2-nd task

--- Practice 2.js
+++ Practice 2.js
@@ -1,9 +1,17 @@
 
-let array = [];
-function(array, target) {
-	for (i = 0; i < array.length; i++) {
-		for (var k = 0; k < array.length; k++) {
+function calc(array, target) {
+ for(i=0; i < array.length; i++) {
+  for(k=0; k < array.length; k++) {
+   if (array[i] !== array[k]) {
+    if(target === array[i] + array[k]) {
+    	return [array[i], array[k]];
+    }
+   }
+  }
+ }
 
-		}
-	}
 }
+
+// calc([array], target)
+
+calc([1, 2, 3], 5)
